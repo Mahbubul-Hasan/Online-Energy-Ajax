2e3305502df372a8d0a5786e398b2e6544b8173b
Mahbubul-Hasan <mahbubul161@gmail.com>
2019-09-15 01:15:18 +0600

    Setup App Key

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Online Energy'),
 
     /*
     |--------------------------------------------------------------------------
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Asis/Dhaka',
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', "Fc3VKNHfHmY+mbunn4Q76kYBC1P2e5DcG708mZX/gc0="),
 
     'cipher' => 'AES-256-CBC',
 
