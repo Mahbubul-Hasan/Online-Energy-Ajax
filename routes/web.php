579715fcdc27eda1b0afaed27e529f0f4515eb48
Mahbubul-Hasan <mahbubul161@gmail.com>
2019-09-15 23:25:03 +0600

    Link URL in Dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::group(['prefix' => 'admin','namespace' => 'Admin'], function () {
-    Route::get('/', "DashboardController@index"); 
+Route::group(['prefix' => 'admin','namespace' => 'Admin', 'as' => 'admin.'], function () {
+    Route::get('/', "DashboardController@index")->name('dashboard'); 
 });
