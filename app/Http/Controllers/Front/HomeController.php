41597e84acb64e5b5becd2745671efba4a380634
Mahbubul-Hasan <mahbubul161@gmail.com>
2019-09-28 23:51:57 +0600

    Show offer products in home page & change some css

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('front.home.home');
+        $date["offer_products"] = Product::select("id", "name", "photo", "price", "Offer_price")->where("active", 1)->where("Offer_price", "!=", null)->take(4)->orderBy("id", "desc")->get();
+        return view('front.home.home')->with($date);
+        return $date;
     }
 }
--- resources/views/front/home/home.blade.php
+++ resources/views/front/home/home.blade.php
@@ -19,20 +19,22 @@
 
 
             <div class="con-w3l">
+                @foreach ($offer_products as $offerProduct)              
+                
                 <div class="col-md-3 m-wthree">
                     <div class="col-m">
                         <a href="#" data-toggle="modal" data-target="#myModal1" class="offer-img">
-                            <img src="{{ asset("/") }}asset/front/images/of.png" class="img-responsive" alt="">
+                            <img src="{{ $offerProduct->photo }}" class="img-responsive" alt="" style="width: 100%">
                             <div class="offer">
                                 <p><span>Offer</span></p>
                             </div>
                         </a>
                         <div class="mid-1">
-                            <div class="women">
-                                <h6><a href="single.html">Moong</a>(1 kg)</h6>
+                            <div class="women" style="height: 60px;">
+                                <b><a href="single.html">{{ $offerProduct->name }}</a> (1 kg)</b>
                             </div>
                             <div class="mid-2">
-                                <p><label>$2.00</label><em class="item_price">$1.50</em></p>
+                                <p><label>৳ {{ $offerProduct->price }}</label><strong class="item_price">৳ {{ $offerProduct->Offer_price }}</strong></p>
                                 <div class="block">
                                     <div class="starbox small ghosting"> </div>
                                 </div>
@@ -45,83 +47,14 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-3 m-wthree">
-                    <div class="col-m">
-                        <a href="#" data-toggle="modal" data-target="#myModal2" class="offer-img">
-                            <img src="{{ asset("/") }}asset/front/images/of1.png" class="img-responsive" alt="">
-                            <div class="offer">
-                                <p><span>Offer</span></p>
-                            </div>
-                        </a>
-                        <div class="mid-1">
-                            <div class="women">
-                                <h6><a href="single.html">Sunflower Oil</a>(5 kg)</h6>
-                            </div>
-                            <div class="mid-2">
-                                <p><label>$10.00</label><em class="item_price">$9.00</em></p>
-                                <div class="block">
-                                    <div class="starbox small ghosting"> </div>
-                                </div>
-                                <div class="clearfix"></div>
-                            </div>
-                            <div class="add">
-                                <button class="btn btn-danger my-cart-btn my-cart-b" data-id="2" data-name="Sunflower Oil" data-summary="summary 2" data-price="9.00" data-quantity="1" data-image="{{ asset("/") }}asset/front/images/of1.png">Add to Cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3 m-wthree">
-                    <div class="col-m">
-                        <a href="#" data-toggle="modal" data-target="#myModal3" class="offer-img">
-                            <img src="{{ asset("/") }}asset/front/images/of2.png" class="img-responsive" alt="">
-                            <div class="offer">
-                                <p><span>Offer</span></p>
-                            </div>
-                        </a>
-                        <div class="mid-1">
-                            <div class="women">
-                                <h6><a href="single.html">Kabuli Chana</a>(1 kg)</h6>
-                            </div>
-                            <div class="mid-2">
-                                <p><label>$3.00</label><em class="item_price">$2.00</em></p>
-                                <div class="block">
-                                    <div class="starbox small ghosting"> </div>
-                                </div>
-                                <div class="clearfix"></div>
-                            </div>
-                            <div class="add">
-                                <button class="btn btn-danger my-cart-btn my-cart-b" data-id="3" data-name="Kabuli Chana" data-summary="summary 3" data-price="2.00" data-quantity="1" data-image="{{ asset("/") }}asset/front/images/of2.png">Add to Cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3 m-wthree">
-                    <div class="col-m">
-                        <a href="#" data-toggle="modal" data-target="#myModal4" class="offer-img">
-                            <img src="{{ asset("/") }}asset/front/images/of3.png" class="img-responsive" alt="">
-                            <div class="offer">
-                                <p><span>Offer</span></p>
-                            </div>
-                        </a>
-                        <div class="mid-1">
-                            <div class="women">
-                                <h6><a href="single.html">Soya Chunks</a>(1 kg)</h6>
-                            </div>
-                            <div class="mid-2">
-                                <p><label>$4.00</label><em class="item_price">$3.50</em></p>
-                                <div class="block">
-                                    <div class="starbox small ghosting"> </div>
-                                </div>
-                                <div class="clearfix"></div>
-                            </div>
-                            <div class="add">
-                                <button class="btn btn-danger my-cart-btn my-cart-b" data-id="4" data-name="Soya Chunks" data-summary="summary 4" data-price="3.50" data-quantity="1" data-image="{{ asset("/") }}asset/front/images/of3.png">Add to Cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
+                @endforeach
+
                 <div class="clearfix"></div>
             </div>
+            <a href="#">
+                <p style="text-align: center; margin-top: 10px"><strong>See ALL...</strong></p>
+            </a>
         </div>
     </div>
 
