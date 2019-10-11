435ebd09d9c59c8ea4264f36277e96d58ac624ef
Mahbubul Hasan <mahbubul161@gmail.com>
2019-10-11 22:17:13 +0600

    Get cart product into Checkout

--- app/Http/Controllers/Front/CheckoutController.php
+++ app/Http/Controllers/Front/CheckoutController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers\Front;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Gloudemans\Shoppingcart\Facades\Cart;
 
 class CheckoutController extends Controller
 {
     public function checkoutView()
     {
-        return view("front.checkout.checkout");
+        $data["cart_count"] = Cart::count();
+        $data["cart_products"] = Cart::content();
+
+        return view("front.checkout.checkout")->with($data);
     }
 }
--- resources/views/front/checkout/checkout.blade.php
+++ resources/views/front/checkout/checkout.blade.php
@@ -29,37 +29,20 @@ Checkout
         <div class="col-md-4 order-md-2 mb-4">
             <h2 class="d-flex justify-content-between align-items-center mb-3">
                 <span class="text-muted" >Your cart</span>
-                <span class="badge badge-secondary badge-pill">3</span>
+                <span class="badge badge-secondary badge-pill">{{ $cart_count }}</span>
             </h2>
             <ul class="list-group mb-3">
-                <li class="list-group-item d-flex justify-content-between lh-condensed">
-                    <div>
-                        <h6 class="my-0">Product name</h6>
-                        <small class="text-muted">Brief description</small>
-                    </div>
-                    <span class="text-muted">$12</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between lh-condensed">
-                    <div>
-                        <h6 class="my-0">Second product</h6>
-                        <small class="text-muted">Brief description</small>
-                    </div>
-                    <span class="text-muted">$8</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between lh-condensed">
-                    <div>
-                        <h6 class="my-0">Third item</h6>
-                        <small class="text-muted">Brief description</small>
-                    </div>
-                    <span class="text-muted">$5</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between bg-light">
-                    <div class="text-success">
-                        <h6 class="my-0">Promo code</h6>
-                        <small>EXAMPLECODE</small>
-                    </div>
-                    <span class="text-success">-$5</span>
-                </li>
+              @foreach ($cart_products as $product)
+                  
+              <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <div>
+                      <h6 class="my-0">{{ $product->name }}</h6>
+                      <small class="text-muted">৳ {{ $product->price }} * {{ $product->qty }}</small>
+                  </div>
+                  <span class="text-muted">৳{{ $product->price * $product->qty }}</span>
+              </li>
+
+              @endforeach
                 <li class="list-group-item d-flex justify-content-between">
                     <span>Total (USD)</span>
                     <strong>$20</strong>
